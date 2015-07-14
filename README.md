d3a9a6fd17c3a33a4f0315c896cf16ec92c4308f
Yintao Song <yintaosong@gmail.com>
2015-07-13 21:21:06 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Canon #
+
+Canon is a python package for X-Ray Laue diffractometer analysis.
+
+Download and upzip the repository. Install the package by running the following in the unzipped folder:
+
+    python setup.py install
+
+Then import the library in your python script
+
+    import canon
+
+This is a small project maintained by only a tiny group,
+therefore does not have enough human resource to keep up a comprehensive documentation.
+Hopefully, the code is sufficiently self-explanatory.
+
+## Dependencies
+
+### Required
+
+- Numpy >= 1.6.0
+
+### Optional
+
+- Scipy >= 0.11.0
+- numba
+
+## Licence
+
+[The MIT Lincense](http://opensource.org/licenses/MIT)
---
+++ setup.py
@@ -0,0 +1,34 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import canon
+
+setup(name='Canon',
+      version=canon.__verison__,
+      description='A python package for X-Ray Laue diffractometer',
+      url='http://github.com/structrans/Canon',
+      author=('Xian Chen', 'Yintao Song'),
+      author_email=('xianchen@ust.hk', 'yintaosong@gmail.com'),
+      license='LICENSE',
+      classifiers = (
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: Freeware',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: Implementation :: CPython'
+      ),
+      packages=(
+          'canon'
+      ),
+      scripts = [],
+      install_requires=(
+          'numpy >= 1.9.0'
+      ),
+      test_suite='nose.collector',
+      tests_require=['nose']
+      )
